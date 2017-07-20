04afd2b23d4a3724be9721dec621417f1fdd3444
Philip W. Howard <phil.w.howard@gmail.com>
2017-07-20 09:15:25 -0700

    Displays selected word in edit area

--- editdict/src/editdict/EditFrame.java
+++ editdict/src/editdict/EditFrame.java
@@ -1,5 +1,6 @@
 package editdict;
 
+import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -38,8 +39,25 @@ public class EditFrame extends JFrame {
 
 	protected class EditPanel extends JPanel
 	{
+		private static final long serialVersionUID = 9071674198967674856L;
+		protected Word m_word;
+		protected WordList m_wordList;
+		protected JTextField m_English;
+		protected JTextField m_French;
+		protected JTextField m_category;
+		protected JRadioButton m_singular;
+		protected JRadioButton m_plural;
+		protected JRadioButton m_masculine;
+		protected JRadioButton m_feminine;
+		protected JRadioButton m_noun;
+		protected JRadioButton m_verb;
+		protected JTable m_wordTable;
+		
 		public EditPanel(WordList list)
 		{
+			m_wordList = list;
+			m_word = m_wordList.get(0);
+			
 			GridBagLayout layout = new GridBagLayout();
 			setLayout(layout);
 			GridBagConstraints c = new GridBagConstraints();
@@ -55,7 +73,8 @@ public class EditFrame extends JFrame {
 			c.fill = GridBagConstraints.HORIZONTAL;
 			c.gridwidth = 3;
 			c.weightx = 1;
-			add(new JTextField(), c);
+			m_English = new JTextField();
+			add(m_English, c);
 
 			c.gridx = 0;
 			c.gridy = 1;
@@ -68,7 +87,8 @@ public class EditFrame extends JFrame {
 			c.fill = GridBagConstraints.HORIZONTAL;
 			c.gridwidth = 3;
 			c.weightx = 1;
-			add(new JTextField(), c);	
+			m_French = new JTextField();
+			add(m_French, c);	
 
 			//***************************************
 			c.gridx = 0;
@@ -77,20 +97,20 @@ public class EditFrame extends JFrame {
 			c.gridwidth = 1;
 			c.weightx = 0;
 			c.anchor = GridBagConstraints.WEST;
-			JRadioButton singular = new JRadioButton("singular");
-			add(singular, c);
+			m_singular = new JRadioButton("singular");
+			add(m_singular, c);
 
 			c.gridx = 0;
 			c.gridy = 3;
 			c.fill = GridBagConstraints.NONE;
 			c.weightx = 0;
 			c.anchor = GridBagConstraints.WEST;
-			JRadioButton plural = new JRadioButton("plural");
-			add(plural, c);
+			m_plural = new JRadioButton("plural");
+			add(m_plural, c);
 
 			ButtonGroup numberGroup = new ButtonGroup();
-			numberGroup.add(singular);
-			numberGroup.add(plural);
+			numberGroup.add(m_singular);
+			numberGroup.add(m_plural);
 
 			//***************************************
 			c.gridx = 1;
@@ -98,20 +118,20 @@ public class EditFrame extends JFrame {
 			c.fill = GridBagConstraints.NONE;
 			c.weightx = 0;
 			c.anchor = GridBagConstraints.WEST;
-			JRadioButton masculine = new JRadioButton("masculine");
-			add(masculine, c);
+			m_masculine = new JRadioButton("masculine");
+			add(m_masculine, c);
 
 			c.gridx = 1;
 			c.gridy = 3;
 			c.fill = GridBagConstraints.NONE;
 			c.weightx = 0;
 			c.anchor = GridBagConstraints.WEST;
-			JRadioButton feminine = new JRadioButton("feminine");
-			add(feminine, c);
+			m_feminine = new JRadioButton("feminine");
+			add(m_feminine, c);
 
 			ButtonGroup genderGroup = new ButtonGroup();
-			genderGroup.add(masculine);
-			genderGroup.add(feminine);
+			genderGroup.add(m_masculine);
+			genderGroup.add(m_feminine);
 
 			//***************************************
 			c.gridx = 2;
@@ -119,20 +139,20 @@ public class EditFrame extends JFrame {
 			c.fill = GridBagConstraints.NONE;
 			c.weightx = 0;
 			c.anchor = GridBagConstraints.WEST;
-			JRadioButton noun = new JRadioButton("noun");
-			add(noun, c);
+			m_noun = new JRadioButton("noun");
+			add(m_noun, c);
 
 			c.gridx = 2;
 			c.gridy = 3;
 			c.fill = GridBagConstraints.NONE;
 			c.weightx = 0;
 			c.anchor = GridBagConstraints.WEST;
-			JRadioButton verb = new JRadioButton("verb");
-			add(verb, c);
+			m_verb = new JRadioButton("verb");
+			add(m_verb, c);
 
 			ButtonGroup partGroup = new ButtonGroup();
-			genderGroup.add(noun);
-			genderGroup.add(verb);
+			partGroup.add(m_noun);
+			partGroup.add(m_verb);
 
 			//***************************************
 			c.gridx = 3;
@@ -148,7 +168,8 @@ public class EditFrame extends JFrame {
 			c.weightx = 0;
 			c.anchor = GridBagConstraints.WEST;
 			c.weightx = 1;
-			add(new JTextField(), c);	
+			m_category = new JTextField();
+			add(m_category, c);	
 
 			//***************************************
 			c.gridx = 0;
@@ -164,20 +185,46 @@ public class EditFrame extends JFrame {
 			{
 				colNames.add(item);
 			}
-			JTable wordList = new JTable(new WordListTableModel(list));
-			wordList.setFillsViewportHeight(true);
+			m_wordTable = new JTable(new WordListTableModel(list));
+			m_wordTable.setFillsViewportHeight(true);
 			ListSelectionModel listSelectionModel;
-			listSelectionModel = wordList.getSelectionModel();
+			listSelectionModel = m_wordTable.getSelectionModel();
 			listSelectionModel.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 			listSelectionModel.addListSelectionListener(new SharedListSelectionHandler());
-			wordList.setSelectionModel(listSelectionModel);
-
-			JScrollPane scrollPane = new JScrollPane(wordList);
+			m_wordTable.setSelectionModel(listSelectionModel);
+			
+			m_wordTable.setRowSelectionInterval(0, 0);
+			
+			JScrollPane scrollPane = new JScrollPane(m_wordTable);
 			add(scrollPane, c);
 
 		}
-		private static final long serialVersionUID = 9071674198967674856L;
+		
+		@Override
+		protected void paintComponent(Graphics g)
+		{
+			m_English.setText(m_word.english);
+			m_French.setText(m_word.french);
+			m_category.setText(m_word.category);
+			
+			if (m_word.number == Word.Number_t.singular)
+				m_singular.setSelected(true);
+			else if (m_word.number == Word.Number_t.plural)
+				m_plural.setSelected(true);
+			
+			if (m_word.gender == Word.Gender_t.masculine)
+				m_masculine.setSelected(true);
+			else if (m_word.gender == Word.Gender_t.feminine)
+				m_feminine.setSelected(true);
+			
+			if (m_word.part == Word.SpeachPart_t.noun)
+				m_noun.setSelected(true);
+			else if (m_word.part == Word.SpeachPart_t.verb)
+				m_verb.setSelected(true);
 
+			super.paintComponent(g);
+		}
+		
 		protected class SharedListSelectionHandler implements ListSelectionListener {
 			public void valueChanged(ListSelectionEvent e) { 
 				ListSelectionModel lsm = (ListSelectionModel)e.getSource();
@@ -211,8 +258,11 @@ public class EditFrame extends JFrame {
 					for (int i = minIndex; i <= maxIndex; i++) {
 						if (lsm.isSelectedIndex(i)) {
 							System.out.println("selected: " + i);
+							m_word = m_wordList.get(i);
+							break;
 						}
 					}
+					repaint();
 					
 				}
 			}
